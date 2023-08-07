53dc23990d6d8bae25964138def7ea07b078f618
Lisardgy <key.cmdark@gmail.com>
2023-08-07 20:03:13 +0700

    feat: init nuxt repo

---
+++ components/lorem20.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-blue-500">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque explicabo
+    id exercitationem nam, deserunt corrupti cumque aliquam. Facere, veniam
+    voluptates ipsa unde itaque provident non ab perspiciatis omnis quam quidem.
+  </div>
+</template>
